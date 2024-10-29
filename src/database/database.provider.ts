2ce2379e2c28f2ab4fb10221439840cf935e8449
xjado <xjadovibralex@gmail.com>
2024-10-28 19:36:42 -0500

    nuevoactual

--- src/database/database.provider.ts
+++ src/database/database.provider.ts
@@ -17,7 +17,7 @@ export const DatabaseProvider:DynamicModule =TypeOrmModule.forRootAsync({
             password: config.get('DB_PASSWORD'),
             database: 'twiter-api',
             autoLoadEntities: true,
-            synchronize: true
+            // synchronize: true
 
         } as TypeOrmModuleOptions;
 
